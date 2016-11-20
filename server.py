95b8590d486207a31f57c3db5dec6dcaf84fe370
JulianKnodt <julianknodt@gmail.com>
2016-11-19 18:05:46 -0800

    Changed file structure

--- server.py
+++ server.py
@@ -1,19 +1,9 @@
 from flask import Flask
-app = Flask("j")
-
-def increment(x):
-  return x+1;
-
-print(map(increment, [1,2,3]));
-
+app = Flask(__name__,static_url_path='')
 
 @app.route("/")
 def hello():
     return app.send_static_file('index.html')
-
-@app.route("/foo")
-def yo():
-    return "foobar"
 
 if __name__ == "__main__":
     app.run()
